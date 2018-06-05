84680288c7bc19371cd21c587dd9568809accc1d
github username <github email address>
2018-06-05 16:15:33 -0400

    blank file, still needs work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -8,7 +8,12 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do
+    erb :new
+  end
 
+  post '/posts' do
+    Post.create(params)
+    erb :index
   end
 end
