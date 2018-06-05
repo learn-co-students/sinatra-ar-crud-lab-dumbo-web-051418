9b516e75575ed6cc5c270024aaacd29d9d7ab0b0
github username <github email address>
2018-06-05 19:09:44 -0400

    implemented blogging system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,43 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  post '/posts' do
+    Post.create(params)
+    redirect '/posts'
   end
 
   get '/posts/new' do
     erb :new
   end
 
-  post '/posts' do
-    Post.create(params)
-    erb :index
+  get "/posts/:id" do
+    @post = Post.find(params[:id])
+    erb :specific_post
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch "/posts/:id" do
+    @post = Post.find(params[:id])
+    param_copy = params.dup
+    param_copy.delete "id"
+    param_copy.delete "_method"
+    Post.update(param_copy)
+    redirect '/posts'
+  end
+
+  delete "/posts/:id" do
+    Post.find(params[:id]).destroy
+    redirect '/posts'
   end
 end
